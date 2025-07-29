1ab847e8a3f7b1fb5841507b32302a3fefbbc651
prsonda <prsonda@gmail.com>
2025-07-29 19:59:09 -0300

    feat: setup Swagger documentation with JWT Bearer auth

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../package.json';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './infra/http/filters/all-exceptions.filter';
 import { SecureMiddleware } from './infra/http/middlewares/secure.middleware';
 import { logger } from './infra/logger/logger.service';
+import { DocsMessages } from './shared/constants/messages';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -22,6 +25,23 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
+
+  const options = new DocumentBuilder()
+    .setTitle(DocsMessages.main.title)
+    .setDescription(DocsMessages.main.description)
+    .setVersion(version)
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'Authorization',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
