1e3d3eab1419def36d4cefcb50095ea31d43dd91
prsonda <prsonda@gmail.com>
2025-07-29 19:01:12 -0300

    feat: add global exception filter with basic error logging

---
+++ src/infra/http/filters/all-exceptions.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Erro interno do servidor';
+
+    const logger = new Logger(AllExceptionsFilter.name);
+    logger.error(exception);
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+    });
+  }
+}
