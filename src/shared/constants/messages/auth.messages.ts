86231d55a18e0423d4772508e2d0310dd09d70e8
prsonda <prsonda@gmail.com>
2025-08-01 12:56:11 -0300

    chore(messages): add new auth-related messages

--- src/shared/constants/messages/auth.messages.ts
+++ src/shared/constants/messages/auth.messages.ts
@@ -1,4 +1,7 @@
 export const AuthMessages = {
   loginSuccess: 'Login successful',
   invalidCredentials: 'Invalid credentials',
+  emailDescription: 'User email address',
+  passwordDescription: 'User password',
+  tokenDescription: 'Access token for authentication',
 };
